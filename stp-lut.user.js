8981910495abe9e9fdbf9b722f004afe564b09c8
John Harrison <dpkg_deb@yahoo.com>
2012-07-06 20:14:38 +0100

    Fist chicken

---
+++ stp-lut.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           STP-LUT
+// @namespace      http://www.thetrainline.com/buytickets/?
+// @description    Fill in Trainline
+// @include        http://www.thetrainline.com/buytickets/?
+// ==/UserScript==
+
+document.getElementById ("OriginStation").value = "London St Pancras Intl";
+document.getElementById ("DestinationStation").value = "Luton";
+
